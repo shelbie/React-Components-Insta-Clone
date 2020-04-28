09736e52962148899257c128797f292a51aa481b
Shelbie Taylor <shelbieltaylor@gmail.com>
2020-04-28 14:54:02 -0500

    added to postscontainer

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,5 +1,5 @@
 // You will add code in this file
-import React, { useState } from "react";
+import React from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -9,7 +9,7 @@ import "./Posts.css";
 // pass props in this file to
 const Post = props => {
   // set up state for the likes
-  const [likes, setLikes] = useState(0);
+  console.log(props)
 
   return (
     <div className="post-border">
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,16 +4,17 @@ import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
 import dummyData from "../../dummy-data";
+
 // import data 
 
 const PostsPage = () => {
   // set up state for your data
-  const [data, setData] = useState();
+  const [data] = useState(dummyData);
   return (
     <div className="posts-container-wrapper">
-      {dummyData.map(post => {
-        return post.value;
-      })}
+      {data.map(p => (<Post post = {p} />
+      ))}
+    
     </div>
   );
 };
